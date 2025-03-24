42e200a921b4ec55391596041242afd17ca83808
Gino Settecasi <48999853+ginosettecasi@users.noreply.github.com>
2025-03-24 17:40:27 -0400

    Update cloudfront.tf

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -3,8 +3,22 @@ resource "random_id" "id" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "secure-edge-bucket-${random_id.id.hex}"
-  acl    = "public-read"
+  bucket         = "secure-edge-bucket-${random_id.id.hex}"
+  force_destroy  = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.ownership]
+  bucket     = aws_s3_bucket.website_bucket.id
+  acl        = "private"
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
