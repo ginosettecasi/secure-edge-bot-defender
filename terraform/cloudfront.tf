5c16aaef3c573ca874c422ae5b03ca8956ce39b4
Gino Settecasi <48999853+ginosettecasi@users.noreply.github.com>
2025-03-24 17:27:25 -0400

    Update cloudfront.tf

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -5,9 +5,13 @@ resource "aws_cloudfront_origin_access_identity" "default" {
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "secure-edge-bucket-${random_id.id.hex}"
   acl    = "public-read"
+}
 
-  website {
-    index_document = "index.html"
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
   }
 }
 
