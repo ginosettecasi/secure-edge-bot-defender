a5e8a7b206475b58ffb46155235e1d8c4c3405dd
Gino Settecasi <48999853+ginosettecasi@users.noreply.github.com>
2025-03-24 17:25:15 -0400

    Update waf.tf

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -1,6 +1,6 @@
 resource "aws_wafv2_web_acl" "edge_security_acl" {
   name        = "secure-edge-acl"
-  description = "Edge security WAF for CloudFront"
+  description = "Enhanced edge security WAF for bot mitigation"
   scope       = "CLOUDFRONT"
 
   default_action {
@@ -13,14 +13,13 @@ resource "aws_wafv2_web_acl" "edge_security_acl" {
     sampled_requests_enabled   = true
   }
 
+  # Block known bad user-agents
   rule {
     name     = "block-bad-user-agents"
     priority = 0
-
     action {
       block {}
     }
-
     statement {
       byte_match_statement {
         field_to_match {
@@ -36,7 +35,6 @@ resource "aws_wafv2_web_acl" "edge_security_acl" {
         }
       }
     }
-
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "blockBadBots"
@@ -44,19 +42,69 @@ resource "aws_wafv2_web_acl" "edge_security_acl" {
     }
   }
 
+  # Block requests missing Accept-Language (bots rarely send this)
   rule {
-    name     = "rate-limit-login"
+    name     = "missing-accept-language"
     priority = 1
-
     action {
       block {}
     }
+    statement {
+      not_statement {
+        statement {
+          byte_match_statement {
+            field_to_match {
+              single_header {
+                name = "accept-language"
+              }
+            }
+            positional_constraint = "EXISTS"
+            search_string         = ""
+            text_transformation {
+              priority = 0
+              type     = "LOWERCASE"
+            }
+          }
+        }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "missingAcceptLanguage"
+      sampled_requests_enabled   = true
+    }
+  }
 
+  # Block countries (geo match example)
+  rule {
+    name     = "geo-block"
+    priority = 2
+    action {
+      block {}
+    }
+    statement {
+      geo_match_statement {
+        country_codes = ["CN", "RU", "KP", "IR"]
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "geoBlock"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Rate limit /login
+  rule {
+    name     = "rate-limit-login"
+    priority = 3
+    action {
+      block {}
+    }
     statement {
       rate_based_statement {
         limit              = 100
         aggregate_key_type = "IP"
-
         scope_down_statement {
           byte_match_statement {
             field_to_match {
@@ -72,10 +120,38 @@ resource "aws_wafv2_web_acl" "edge_security_acl" {
         }
       }
     }
-
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "rateLimitLogin"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Custom Header Rule - triggered by Lambda@Edge
+  rule {
+    name     = "block-lambda-flagged"
+    priority = 4
+    action {
+      block {}
+    }
+    statement {
+      byte_match_statement {
+        field_to_match {
+          single_header {
+            name = "x-suspicious-bot"
+          }
+        }
+        positional_constraint = "EXACTLY"
+        search_string         = "true"
+        text_transformation {
+          priority = 0
+          type     = "LOWERCASE"
+        }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "lambdaFlaggedBots"
       sampled_requests_enabled   = true
     }
   }
