bc1a537b530b0f6e82fa50278942530f416e48ec
Gino Settecasi <48999853+ginosettecasi@users.noreply.github.com>
2025-03-24 17:15:36 -0400

    Create cloudfront.tf

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,62 @@
+resource "aws_cloudfront_origin_access_identity" "default" {
+  comment = "Access Identity for Secure Edge Project"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "secure-edge-bucket-${random_id.id.hex}"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "random_id" "id" {
+  byte_length = 4
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.website_endpoint
+    origin_id   = "s3-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CloudFront CDN with WAF"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  web_acl_id = aws_wafv2_web_acl.edge_security_acl.arn
+}
