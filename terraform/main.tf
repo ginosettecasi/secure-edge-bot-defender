a2a01a4b630adad140dc2e030c0d3c6c759b85c4
Gino Settecasi <48999853+ginosettecasi@users.noreply.github.com>
2025-03-24 17:14:49 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 
+  required_version = ">= 1.4.0"
+
+  backend "local" {}
+}
+
+provider "aws" {
+  region = "us-east-1" # Required for CloudFront
+}
