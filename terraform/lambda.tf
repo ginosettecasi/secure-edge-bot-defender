eab18484e46019095bc5b2b4b8d972b833546e15
Gino Settecasi <48999853+ginosettecasi@users.noreply.github.com>
2025-03-24 17:55:41 -0400

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -13,6 +13,12 @@ resource "aws_iam_role" "lambda_edge_role" {
   assume_role_policy = data.aws_iam_policy_document.lambda_edge_assume_role.json
 }
 
+data "archive_file" "bot_fingerprint" {
+  type        = "zip"
+  source_dir  = "${path.module}/../lambda"
+  output_path = "${path.module}/../lambda/bot_fingerprint.zip"
+}
+
 resource "aws_lambda_function" "bot_detector" {
   function_name = "bot-fingerprint-detector"
   role          = aws_iam_role.lambda_edge_role.arn
@@ -24,22 +30,4 @@ resource "aws_lambda_function" "bot_detector" {
   timeout       = 3
 
   source_code_hash = data.archive_file.bot_fingerprint.output_base64sha256
-}
-
-data "archive_file" "bot_fingerprint" {
-  type        = "zip"
-  source_dir  = "${path.module}/../lambda"
-  output_path = "${path.module}/../lambda/bot_fingerprint.zip"
-}
-
-resource "aws_cloudfront_distribution" "cdn" {
-  # 🔁 Use the existing resource block you already have
-  # Add this inside the default_cache_behavior:
-  default_cache_behavior {
-    lambda_function_association {
-      event_type   = "viewer-request"
-      lambda_arn   = aws_lambda_function.bot_detector.qualified_arn
-      include_body = false
-    }
-  }
 }
