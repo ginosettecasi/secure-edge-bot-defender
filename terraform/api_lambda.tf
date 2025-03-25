37e7e654a9a3efb643ba57303b9d0c500b5ace98
Gino Settecasi <48999853+ginosettecasi@users.noreply.github.com>
2025-03-24 23:26:03 -0400

    Update api_lambda.tf

--- terraform/api_lambda.tf
+++ terraform/api_lambda.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# IAM Role
 resource "aws_iam_role" "api_lambda_role" {
   name = "api-bot-detector-role"
 
@@ -10,68 +9,64 @@ resource "aws_iam_role" "api_lambda_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Principal = {
           Service = "lambda.amazonaws.com"
-        },
+        }
         Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
-# Logging Policy
 resource "aws_iam_role_policy" "api_lambda_logging" {
   name = "lambda-logging"
   role = aws_iam_role.api_lambda_role.id
 
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
       {
         Action = [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents"
-        ],
-        Effect = "Allow",
+        ]
+        Effect   = "Allow"
         Resource = "*"
       }
     ]
   })
 }
 
-# Package Lambda
 data "archive_file" "bot_fingerprint_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../lambda"
   output_path = "${path.module}/../lambda/bot_fingerprint.zip"
 }
 
-# Lambda Function
 resource "aws_lambda_function" "bot_fingerprint_api" {
-  function_name = "api-bot-detector"
-  filename      = data.archive_file.bot_fingerprint_zip.output_path
+  function_name    = "api-bot-detector"
+  filename         = data.archive_file.bot_fingerprint_zip.output_path
   source_code_hash = data.archive_file.bot_fingerprint_zip.output_base64sha256
 
-  handler = "bot_fingerprint.lambda_handler"
-  runtime = "python3.11"
-  role    = aws_iam_role.api_lambda_role.arn
-  timeout = 3
+  handler     = "bot_fingerprint.lambda_handler"
+  runtime     = "python3.11"
+  role        = aws_iam_role.api_lambda_role.arn
+  timeout     = 3
   memory_size = 128
 }
 
-# API Gateway
 resource "aws_apigatewayv2_api" "bot_api" {
   name          = "bot-detector-api"
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.bot_api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.bot_fingerprint_api.invoke_arn
-  integration_method = "POST"
+  api_id                 = aws_apigatewayv2_api.bot_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.bot_fingerprint_api.invoke_arn
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 
@@ -87,7 +82,6 @@ resource "aws_apigatewayv2_stage" "prod" {
   auto_deploy = true
 }
 
-# Lambda Permission to Allow API Gateway to Invoke
 resource "aws_lambda_permission" "api_gateway_invoke" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
